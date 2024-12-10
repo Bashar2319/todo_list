c70f1dc15f188aae9be44633fb17b18dbaa907ee
bashar <nizambashar10@gmail.com>
2024-12-10 20:00:41 +0530

    To_do list

--- Megablog/src/App.jsx
+++ Megablog/src/App.jsx
@@ -1,14 +1,38 @@
 
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwriter/auth"
+import { login ,logout} from './store/authslice'
+
+
+
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);  
-
-  return (
-    <>
-     <h1>Appwrite</h1>
-    </>
-  )
+ const [loading , setLoading]=useState(true)
+ const dispatch = useDispatch()
+ 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())}
+    })
+    .finally(() =>setLoading(false) )
+  },[])
+ 
+ return !loading ? (
+ <div>
+  <div>
+    <header/>
+    <main>
+      {/* <Outlet */}
+    </main>
+    <footer/>
+  </div>
+ </div>   
+  ) : null
 }
 
 export default App
---
+++ Megablog/src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import conf from "../conf/conf.js";
+import { Client ,Account, ID } from "appwrite";
+export class AuthService{
+    client = new Client();
+    account ;
+
+    constructor(){
+        this.client
+            .setEndpoint (conf.appwriteUrl);
+            .setProject (conf.appwriterProjectId) ;
+            this.account = new Account;
+}
+    async createAccount({email, password, name}){
+        try {
+            const useraccount = await this.account.create(ID, email, password ,name)
+            if (userAccount){
+                return this.login({email, password});
+            }
+            else{
+                return userAccount    ;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email, password,}){
+        try {
+            await this.account.createEmailSession(email, password)
+        } catch (error) {
+           throw error ;
+        }
+    }
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+
+}
+const authService = new AuthService();
+export default authService;
+ 
